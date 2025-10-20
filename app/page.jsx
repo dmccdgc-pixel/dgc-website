cb05ed4aad034fb9696e68b5c84b5e6c996cbe1d
dmccdgc-pixel <dmcc.dgc@gmail.com>
2025-10-20 18:13:56 +0800

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        backgroundColor: "black",
+        color: "gold",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontFamily: "sans-serif",
+      }}
+    >
+      <h1>Welcome to Dubai Gold Coin (DGC)</h1>
+    </main>
+  );
+}
